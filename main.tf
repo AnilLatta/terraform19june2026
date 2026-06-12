2761194514f24fae06d0084932d5cc907f53a26e
anil <akanilkumarsharma@b18g12.online>
2026-06-12 17:51:51 +0000

    add

---
+++ main.tf
@@ -0,0 +1,12 @@
+resource "azurerm_resource_group" "rg1" {
+  name     = "rg1"
+  location = "West Europe"
+}
+resource "azurerm_resource_group" "rg2" {
+  name     = "rg2"
+  location = "central india"
+}
+resource "azurerm_resource_group" "rg3" {
+  name     = "rg3"
+  location = "West us"
+}
