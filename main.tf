34c13f29d458d1ab599dc6c84de6fbd72b3e5dbd
anil <akanilkumarsharma@b18g12.online>
2026-06-15 20:10:56 +0000

    newuptodate1

--- main.tf
+++ main.tf
@@ -11,10 +11,16 @@ resource "azurerm_resource_group" "rg3" {
   location = "West us"
 }
 
-resource "azurerm_storage_account" "stgs"{
-  name = "storga1606"
-  location =azurerm_resource_group.rg1.location
-  resource_group_name = azurerm_resource_group.rg1.name
-  account_tier = "Standard"
+resource "azurerm_resource_group" "vars" {
+  for_each = var.vargs
+
+  name     = each.vale.name
+  location = each.value.location
+}
+resource "azurerm_storage_account" "stgs" {
+  name                     = "storga1606"
+  location                 = azurerm_resource_group.rg1.location
+  resource_group_name      = azurerm_resource_group.rg1.name
+  account_tier             = "Standard"
   account_replication_type = "LRS"
 }
---
+++ variables.tf
@@ -0,0 +1,2 @@
+variable "vargs" {}
+
