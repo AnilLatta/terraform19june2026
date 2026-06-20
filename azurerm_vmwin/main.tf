747d0ac7bd7c738805c8a2e5f7c41fc9fae7157c
anil <akanilkumarsharma@b18g12.online>
2026-06-20 16:03:57 +0000

    vmwin

---
+++ azurerm_vmwin/main.tf
@@ -0,0 +1,24 @@
+resource "azurerm_windows_virtual_machine" "vmwin" {
+  for_each = var.vmwin
+  name                = each.value.name
+  resource_group_name = each.value.resource_group_name
+  location            = each.value.location
+  size                = each.value.size
+  admin_username      = each.value.admin_username
+  admin_password      = each.value.admin_password
+  network_interface_ids = [
+    azurerm_network_interface.example.id,
+  ]
+
+  os_disk {
+    caching              = each.value.os_disk.caching
+    storage_account_type = each.value.os_disk.storage_account_type
+  }
+
+  source_image_reference {
+    publisher = each.value.source_image_reference.publisher
+    offer     = each.value.source_image_reference.offer
+    sku       = each.value.source_image_reference.sku
+    version   = each.value.source_image_reference.version
+  }
+}
---
+++ azurerm_vmwin/terraform.tfvars
@@ -0,0 +1,25 @@
+vmwin = {
+  vmwin1 = {
+  name                = "vmwin1"
+  resource_group_name = "rg1"
+  location            = a"japaneast"
+  size                = "Standard_D2_v5"
+  admin_username      = "adminuser"
+  admin_password      = "P@$$w0rd1234!"
+  network_interface_ids = [
+    azurerm_network_interface.example.id,
+  ]
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "MicrosoftWindowsServer"
+    offer     = "WindowsServer"
+    sku       = "2016-Datacenter"
+    version   = "latest"
+  }
+}
+}
